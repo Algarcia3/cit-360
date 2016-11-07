11ef09438825940a041409004a05dbc4bb552a9a
Algarcia3 <Alfredo.garcia.284@my.csun.edu>
2016-11-07 02:18:31 -0800

    Working terraform code

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,4 +1,3 @@
-
 variable "vpc_id" {
   description = "VPC ID for usage throughout the build process"
   default = "vpc-cf1b05ab"
@@ -16,6 +15,40 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+
+#creates private subnets 
+resource "aws_subnet" "private_subnet_a" {
+   vpc_id = "${var.vpc_id}"
+   cidr_block = "172.31.0.0/22"
+   availability_zone = "us-west-2a"
+   map_public_ip_on_launch = false
+   
+  tags { 
+     Name = "private_a"
+   }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+   vpc_id = "${var.vpc_id}"
+   cidr_block = "172.31.4.0/22"
+   availability_zone = "us-west-2b"
+   map_public_ip_on_launch = false
+   
+  tags { 
+     Name = "private_b"
+   }
+}
+     
+resource "aws_subnet" "private_subnet_c" {
+   vpc_id = "${var.vpc_id}"
+   cidr_block = "172.31.8.0/22"
+   availability_zone = "us-west-2c"
+   map_public_ip_on_launch = false
+   
+  tags {  
+     Name = "private_c"
+  }
+}
 resource "aws_route_table" "public_routing_table" {
   vpc_id = "${var.vpc_id}"
   route {
@@ -28,46 +61,23 @@ resource "aws_route_table" "public_routing_table" {
   }
 }
 
+resource "aws_route_table" "private_routing_table" {
+  vpc_id = "${var.vpc_id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.gw.id}"
+  }
 
-#****creates private subnets***** 
-resource "aws_subnet" "private_subnet_a"{
-   vpc_id = "${var.vpc_id}"
-   cidr_block = "172.31.0.0/22"
-   availability_zone = "us-west-2a"
-   map_public_ip_on_launch = false
-   
-  tags { 
-     Name = "private_a"
-   }
-}
-
-resource "aws_subnet" "private_subnet_b"{
-   vpc_id = "${var.vpc_id}"
-   cidr_block = "172.31.4.0/22"
-   availability_zone = "us-west-2b"
-   map_public_ip_on_launch = false
-   
-  tags { 
-     Name = "private_b"
-   }
-}
-     
-resource "aws_subnet" "private_subnet_c"{
-   vpc_id = "${var.vpc_id}"
-   cidr_block = "172.31.8.0/22"
-   availability_zone = "us-west-2c"
-   map_public_ip_on_launch = false
-   
-  tags {  
-     Name = "private_c"
+  tags {
+    Name = "private_routing_table"
   }
 }
           
     
-#*****creates public subnets******    
+#creates public subnets    
 resource "aws_subnet" "public_subnet_a" {
    vpc_id = "${var.vpc_id}"
-   cidr_block = "172.31.10.0/24"
+   cidr_block = "172.31.20.0/24"
    availability_zone = "us-west-2a"
    depends_on = ["aws_internet_gateway.gw"]
    map_public_ip_on_launch = true
@@ -77,23 +87,23 @@ resource "aws_subnet" "public_subnet_a" {
     }
 }
 
-recource "aws_subnet" "public_subnet_b"{
+resource "aws_subnet" "public_subnet_b" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.11.0/24"
-  availability_zone = "us_west-2b"
+  cidr_block = "172.31.21.0/24"
+  availability_zone = "us-west-2b"
   depends_on = ["aws_internet_gateway.gw"]
   map_public_ip_on_launch = true
  
-  tags{ 
+  tags { 
       Name = "public_b"
    }
 }  
      
       
-recourse "aws_subnet" "public_subnet_c"{
+resource "aws_subnet" "public_subnet_c" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "172.31.12.0/24"
-  availability_zone = "us_west-2c"
+  cidr_block = "172.31.22.0/24"
+  availability_zone = "us-west-2c"
   depends_on = ["aws_internet_gateway.gw"]
   map_public_ip_on_launch = true
 
@@ -134,47 +144,38 @@ resource "aws_route_table_association" "private_subnet_b_rt_assoc" {
 resource "aws_route_table_association" "private_subnet_c_rt_assoc" {
   subnet_id = "${aws_subnet.private_subnet_c.id}"
   route_table_id = "${aws_route_table.private_routing_table.id}"
+}
 
-
-resource = "aws_eip" "nat" {
+resource "aws_eip" "nat" {
    vpc = true
 } 
 
 resource "aws_nat_gateway" "gw" {
-   allocation_id = "${aws.eip.nat.id}"
+   allocation_id = "${aws_eip.nat.id}"
    subnet_id = "${aws_subnet.public_subnet_a.id}"
    depends_on = ["aws_internet_gateway.gw"]
 }
 
 
-resource "aws_security_group" "secgrp" {
-  vpc_id = "${var.vpc_id}
+resource "aws_security_group" "group" {
+  vpc_id = "${var.vpc_id}"
 
    ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  
-   egress {
-    from_port = 0
-    to_port = 0 
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+   from_port = 22
+   to_port = 22
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
+}  
 resource "aws_eip" "bastion" { 
-  instance = "{$aws_instance.bastion.id}
+  instance = "${aws_instance.bastion.id}"
   vpc = true
 }
 
 resource "aws_instance" "bastion" {
    ami = "ami-b04e92d0"
    instance_type = "t2.micro"
-   subnet_id = "${aws_subnet_public_subnet_a.id}"
-   sucurity_groups = ["${aws.subnet.public_subnet_a.id}"]
+   subnet_id = "${aws_subnet.public_subnet_a.id}"
+   security_groups = ["${aws_security_group.group.id}"]
    key_name = "cit360"
-
-
-
+}
